a1dee9524128f2382203bab8828ba2b3f0eea95b
selimmartinez <36453094+selimmartinez@users.noreply.github.com>
2018-05-03 18:34:46 -0400

    Add files via upload

---
+++ fizzbuzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for(i=1; i<=100; i++)
+	{
+		if(((i%3)||(i%5))==0)
+			printf("%d FizzBuzz\n", i);
+		else if((i%3)==0)
+			printf("%d Fizz\n", i);
+		else if((i%5)==0)
+			printf("%d Buzz\n", i);
+		else
+			printf("%d\n", i);
+		
+	}
+	
+	return 0;
+	
+}
